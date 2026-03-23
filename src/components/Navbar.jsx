c4dff84b625fcf2ab9a679672bae5bd98728496a
EmmyDan1 <emmydan68@gmail.com>
2026-03-23 13:08:22 +0100

    comit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
       transition={{ duration: 0.6 }}
       className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-6 md:px-12 py-5 bg-[#0a0a0a]/80 backdrop-blur-md border-b border-white/5"
     >
-        <img src={Logo} alt="Westside Clothing" className="h-16 w-auto" />
+        <img src={Logo} alt="Westside Clothing" className="h-14 w-auto" />
     </motion.nav>
   )
 }
